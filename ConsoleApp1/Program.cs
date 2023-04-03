1aa277f8bb287445ac171dda43adc90d52a5d049
MorrisVI <125936660+MorrisVI@users.noreply.github.com>
2023-04-03 18:39:25 -0500

    actuaaaa
    actua el console app 1 XD es el arreglo del de batalla naval aclaro XD

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Donde empezara el barco? ingrese la coordenada horizontal(0) y vertical(1)");
+            string boatPosition = Console.ReadLine();
+            string[] coordinates = boatPosition.Split(',');
+
+
+            Console.WriteLine("Cual sera la longitud del barco?");
+            int varibable1 = (yfilas[0]);
+            Console.WriteLine();
+            int variable2 = (xcolumnas[1]);
+            Console.WriteLine();
+          
+
+
+            int[,] tablero = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var filas = tablero.GetLength(0);
+            var columnas = tablero.GetLength(1);
+
+            var yfilas = 10;
+            var xcolumnas = 10;
+
+            for (int i = 0; i < yfilas; i++)
+            {
+                for (int j = 0; j < xcolumnas; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
